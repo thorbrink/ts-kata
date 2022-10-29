0480ba3f17f9c88cc2a4fcf021b5f95db1db9f03
Thor Brink <thorbrink@gmail.com>
2022-10-29 18:37:01 +0000

    Run readme test conditionally

--- katas/readme.test.ts
+++ katas/readme.test.ts
@@ -5,9 +5,10 @@ const readmeFileName = 'README.md'
 const allFilesInCurrentDirectory = readdirSync(rootPath, { withFileTypes: true })
 const subFolders = allFilesInCurrentDirectory.filter(file => file.isDirectory())
 const subFolderNames = subFolders.map(folder => folder.name)
+const itif = (condition:boolean) => condition ? it : it.skip;
 
 describe('Every kata has a README file', () => {
-    it.each(subFolderNames)(`${rootPath}/%s/${readmeFileName} is present`, (folderName) => {
+    itif(subFolderNames.length > 0).each(subFolderNames)(`${rootPath}/%s/${readmeFileName} is present`, (folderName) => {
         const readmeIsPresentInSubFolder = existsSync(`${rootPath}/${folderName}/${readmeFileName}`)
         expect(readmeIsPresentInSubFolder).toEqual(true)
     })
